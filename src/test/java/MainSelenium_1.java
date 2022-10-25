6d22579f4846c248c3f76dfb4a7c4a5a84cf8a13
Cristian Zidarescu <Cristian Zidarescu>
2022-10-25 22:30:41 +0300

    MainSelenium_1 rev.25.10.2022

--- src/test/java/MainSelenium_1.java
+++ src/test/java/MainSelenium_1.java
@@ -21,30 +21,32 @@ public class MainSelenium_1 {
         WebElement footerElement = driver.findElement(By.tagName("footer")); //
         String footerMsg = footerElement.getText();
         System.out.println(footerMsg);
-        Thread.sleep(2000);
+        Thread.sleep(1500);
         WebElement formsPage= driver.findElement(By.xpath("//div[@class='card mt-4 top-card'][2]"));
         formsPage.click();
-        Thread.sleep(2000);
+        Thread.sleep(1500);
 //  Trecem la urmatoarea pagina
         driver.findElement(By.xpath("//div[@class='header-wrapper'][1]")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1500);
         driver.findElement(By.id("item-3")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1500);
         driver.findElement(By.id("addNewRecordButton")).click();
+        Thread.sleep(1500);
         driver.findElement(By.id("firstName")).sendKeys("Cristian");
-        Thread.sleep(2000);
+        Thread.sleep(1500);
         driver.findElement(By.id("lastName")).sendKeys("Emil");
-//  cu linia de comanda de mai sus se impiedica dupa "lastName:
-//  cu linia de comanda de mai jos se impiedica inainte de "lastName"
-//        driver.findElement(By.xpath("//input[@id='Last Name']")).sendKeys("Emil");
+//      driver.findElement(By.xpath("//input[@id='Last Name']")).sendKeys("Emil"); - alternativa pt randul de mai sus.
+        Thread.sleep(1500);
         driver.findElement(By.id("userEmail")).sendKeys("CristianEmil@hotmail.com");
-        Thread.sleep(2000);
-        driver.findElement(By.id("Age")).sendKeys("50");
-        driver.findElement(By.id("Salary")).sendKeys("21.000");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("input[@id='department']")).sendKeys("Engineering");
-        driver.findElement(By.id("Submit")).click();
-        Thread.sleep(10000);
+        Thread.sleep(1500);
+        driver.findElement(By.id("age")).sendKeys("50");
+        Thread.sleep(1500);
+        driver.findElement(By.id("salary")).sendKeys("21000");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@id='department']")).sendKeys("Engineering");
+        Thread.sleep(1500);
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(15000);
 
         driver.close();
         driver.quit();
