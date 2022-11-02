6c87bd44f8f359383ca9e68f53f6fe037ab5d1cd
Cristian Zidarescu <Cristian Zidarescu>
2022-11-02 22:39:16 +0300

    Selenium2-tema3_refacuta

---
+++ src/test/java/Selenium2/Selenium2_Alarme_fake1.java
@@ -0,0 +1,82 @@
+package Selenium2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium2_Alarme_fake1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        prima_alerta();
+        a_doua_alerta();
+    }
+
+    public static void prima_alerta() {
+        ChromeDriver driver = null;
+        try {
+            driver = Browser_Manager.getChromedriver();
+            driver.get("https://testpages.herokuapp.com/styled/alerts/fake-alert-test.html");
+            driver.manage().window().maximize();
+            WebElement firstButton = driver.findElement(By.id("fakealert"));
+//  alarma falsa, se gaseste cu inspectarea in pagina - se gaseste selectorul si se da click pt verificare
+            firstButton.click();
+            Thread.sleep(1000);
+            WebElement button1_OK = driver.findElement(By.id("dialog-ok"));
+            System.out.println(driver.findElement(By.id("dialog-ok")).getText());
+            Thread.sleep(1000);
+            button1_OK.click();
+            Thread.sleep(1000);
+            System.out.println("E alarma 1 deschisa dupa inchidere ? - " + isAlertOpened(driver));
+
+//  se comenteaza linia de ALERTA = se inchide pagina la final de executie si se redeschide pt testul 2
+//           Alert prima_alerta = driver.switchTo().alert();
+
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+         } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    public static void a_doua_alerta() {
+        ChromeDriver driver = null;
+        try {
+            driver = Browser_Manager.getChromedriver();
+            driver.get("https://testpages.herokuapp.com/styled/alerts/fake-alert-test.html");
+            driver.manage().window().maximize();
+            WebElement secondButton = driver.findElement(By.id("modaldialog"));
+            secondButton.click();
+            Thread.sleep(1000);
+            WebElement button2_OK = driver.findElement(By.id("dialog-ok"));
+            System.out.println(driver.findElement(By.id("dialog-ok")).getText());
+            Thread.sleep(1000);
+            button2_OK.click();
+            Thread.sleep(1000);
+            System.out.println("E alarma 2 deschisa dupa inchidere ? - " + isAlertOpened(driver));
+
+//  se comenteaza linia pt ca nu este nevoie. Se inchide pagina la final
+//          Alert a_doua_alerta = driver.switchTo().alert();
+
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+// aici verificam daca alerta este deschisa sau nu!
+    public static boolean isAlertOpened(ChromeDriver driver){
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
+
+
