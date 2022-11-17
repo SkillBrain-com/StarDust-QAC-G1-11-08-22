7ca01439abbe30b882cc62b0f5469a4985c0d462
Cristian Zidarescu <cristianzidarescu@hotmail.com>
2022-11-17 04:36:32 +0200

    Selenium3 -Tema_casa

---
+++ src/test/java/Selenium3/Utilitati_tema_Selen3.java
@@ -0,0 +1,59 @@
+package Selenium3;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utilitati_tema_Selen3 {
+
+    public static ChromeDriver getChromeDriver(){
+        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver.exe" );
+        return new ChromeDriver();
+    }
+
+    public static ChromeDriver getMobileChromeDriver() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver.exe" );
+        ChromeOptions options = new ChromeOptions();
+        Map<String,String> mobileMap= new HashMap<>();
+        mobileMap.put("deviceName" , "PUNE NUMELE DEVICE-ului AICI");
+        options.setExperimentalOption( "mobileEmulation", mobileMap  );
+        options.addArguments("start-maximized");    // maximizarea paginii
+        return new ChromeDriver(options);
+    }
+
+    public static ChromeDriver getChromeImplicitWait(int duration) {
+        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver.exe" );
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(duration));
+        return driver;
+    }
+
+    public static ChromeDriver getChromeWithWait(int duration){
+        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver.exe" );
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(duration));
+        return driver;
+    }
+
+    public static void takeScrennshot (String testcaseName, WebDriver driver) {
+        String destination = null;
+        try {
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File source = screenshot.getScreenshotAs(OutputType.FILE);
+            destination = System.getProperty("user.dir") + "/Logs/" + testcaseName + " .png";
+            FileUtils.copyFile(source, new File(destination));
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("Screenshot taken");
+        }
+    }
+}
